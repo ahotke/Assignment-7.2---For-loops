430d7c2b3d0372f462056a9941cadbbf773f6d19
Ashton Hotke <339741886@GoTVDSB.ca>
2024-10-04 09:32:54 -0400

    Add project files.

---
+++ Assignment 7.2 - For loops/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Assignment_7._2___For_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int y, timesRepeated = 10;
+            string name;
+
+            //Blastoff
+            for (int i = 10; i >= 1; i = i - 1)
+                Console.WriteLine(i);
+
+            Console.WriteLine("Blastoff!");
+            Console.WriteLine();
+
+            //X and Y
+            Console.WriteLine("X" + '\t' + "Y");
+            Console.WriteLine("--------------------");
+            for (int i = -10; i >= 10; i = i + 2)
+            {
+                y = i * i;
+                Console.WriteLine(i + '\t' + y);
+            }
+            Console.WriteLine();
+
+            // Name repeater
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine().ToLower();
+            Console.WriteLine();
+            if (name == "aldworth")
+                timesRepeated = 5;
+
+            for (int i = 0; i <= timesRepeated; i++);
+                Console.WriteLine(name);
+
+        }
+    }
+}
